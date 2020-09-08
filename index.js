b5a6ee64c01ddf4c84c415e4a3b0a1325fa16ed5
Juho Holmi <juhoholmi@gmail.com>
2020-09-08 17:36:51 +0300

    3.17*: puhelinluettelo ja tietokanta, step5 and 3.18*: puhelinluettelo ja tietokanta, step6 done

--- index.js
+++ index.js
@@ -5,50 +5,6 @@ const cors = require('cors')
 const Person = require('./models/person')
 const { request } = require('express')
 
-/* if (process.argv.length < 3) {
-	Person.find({}).then(result => {
-		console.log(`Phonebook:`)
-		result.forEach(person => {
-			console.log(`${person.name} ${person.number}`)
-		})
-		mongoose.connection.close()
-	})
-} else {
-	const name = process.argv[2]
-	const number = process.argv[3]
-	const person = new Person({
-		name: name,
-		number: number
-	})
-
-	person.save().then(response => {
-		console.log(`added ${name} with number ${number} to phonebook`)
-		mongoose.connection.close()
-	})
-}
-let persons = [
-	{ 
-		"name": "Arto Hellas", 
-		"number": "040-123456",
-		"id": 1
-	},
-	{ 
-		"name": "Ada Lovelace", 
-		"number": "39-44-5323523",
-		"id": 2
-	},
-	{ 
-		"name": "Dan Abramov", 
-		"number": "12-43-234345",
-		"id": 3
-	},
-	{ 
-		"name": "Mary Poppendieck", 
-		"number": "39-23-6423122",
-		"id": 4
-	}
-] */
-
 const app = express()
 
 morgan.token('body', function (req, res) { if (req.method === 'POST') {return JSON.stringify(req.body)} return null })
