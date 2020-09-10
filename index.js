277eb63bd10b3f33407dfc9dcbd899c4e747fe59
Juho Holmi <juhoholmi@gmail.com>
2020-09-10 14:20:28 +0300

    removed runvalidators from findByIdAndUpdate

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedPerson => updatedPerson.toJSON())
     .then(updatedAndFormattedPerson => {
       response.json(updatedAndFormattedPerson)
